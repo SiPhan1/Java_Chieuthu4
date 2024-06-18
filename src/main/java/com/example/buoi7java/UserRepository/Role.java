5a0e045860e0344b22718eb20099e66bbc9b4422
SiPhan1 <152263905+SiPhan1@users.noreply.github.com>
2024-06-18 12:12:48 +0700

    Role.java

--- src/main/java/com/example/buoi7java/UserRepository/Role.java
+++ src/main/java/com/example/buoi7java/UserRepository/Role.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum Role {
-    ADMIN(1), // Vai trò quản trị viên, có quyền cao nhất trong hệ thống.
-    USER(2); // Vai trò người dùng bình thường, có quyền hạn giới hạn.
-    public final long value; // Biến này lưu giá trị số tương ứng với mỗi vai trò.
+    ADMIN(1), 
+    USER(2); 
+    public final long value;
 }
